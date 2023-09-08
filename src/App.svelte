95d873d22841eddd8a4d0670da5175040791663e
Aniruddha Damle <aniruddha.damle02@gmail.com>
2023-09-09 03:36:52 +0530

    Built and tested application
    Signed-off-by: Aniruddha Damle <aniruddha.damle02@gmail.com>

---
+++ src/App.svelte
@@ -0,0 +1,35 @@
+<script>
+	let isPressed = false;
+	let pos = {x: 0, y: 0};
+
+  function handleMouseDown(event) {
+		isPressed = true;
+  }
+	function handleMouseMove(event) {
+		if (isPressed == true) {
+			pos.x = event.clientX - 37;
+		  pos.y = event.clientY - 37;
+		}
+	}
+  function handleMouseUp() {
+    isPressed = false;
+  }
+</script>
+
+{@html '<!-- Container for the draggable rectangle -->'}
+<div
+	class = "box"
+	role="presentation"
+	style="width: 50px; 
+				 height: 50px;
+				 background-color: #000000;
+				 position: relative;
+				 top:{pos.y}px;
+				 left:{pos.x}px"
+	on:mousedown={handleMouseDown}
+	on:mousemove={handleMouseMove}
+	on:mouseup={handleMouseUp}
+></div>
+{@html '<!-- Print the coordinates of the rectangle -->'}
+x: {pos.x}; y: {pos.y}
+
